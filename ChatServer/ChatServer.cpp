c08d9b7d470b2698bd430655f1a9a55068d09f65
Oksana Syrova <osyrova.work@gmail.com>
2021-02-26 21:43:09 +0300

    online offline

--- ChatServer/ChatServer.cpp
+++ ChatServer/ChatServer.cpp
@@ -1,9 +1,41 @@
 ﻿#include <iostream>
 #include <uwebsockets/App.h>
+#include <map>
 using namespace std;
 
+//User information
+struct PerSocketData {
+	string name;
+	unsigned int user_id;
+};
+
+map<unsigned int, string> userNames;
+
+const string BROADCAST_CHANNEL = "broadcast";
 const string MESSAGE_TO = "MESSAGE_TO::";
 const string SET_NAME = "SET_NAME::";
+const string OFFLINE = "OFFLINE::";
+const string ONLINE = "ONLINE::";
+
+string online(unsigned int user_id) {
+	string name = userNames[user_id];
+	//check that there is such a user_id in the map!!!
+	return ONLINE + to_string(user_id) + "::" + name;
+}
+
+string offline(unsigned int user_id) {
+	string name = userNames[user_id];
+	//check that there is such a user_id in the map!!!
+	return OFFLINE + to_string(user_id) + "::" + name;
+}
+
+void updateName(PerSocketData* data) {
+	userNames[data->user_id] = data->name;
+}
+
+void deleteName(PerSocketData* data) {
+	userNames.erase(data->user_id);
+}
 
 bool isSetName(string message) {
 	return message.find(SET_NAME) == 0;
@@ -35,30 +67,33 @@ string messageFrom(string user_id, string senderName, string message) {
 }
 
 int main() {
-	//User information
-	struct PerSocketData {
-		string name;
-		unsigned int user_id;
-	};
 
 	unsigned int last_user_id = 10;
-	unsigned int count_user = 0;
 
 	/*Server configuration*/
 	uWS::App(). //create a Server without encryptio
 		ws<PerSocketData>("/*", {
 		/* Settings */
 		.idleTimeout = 1200, //sec
-			.open = [&last_user_id, &count_user](auto* ws) { // "Лямбда функция"
+			.open = [&last_user_id](auto* ws) { // "Лямбда функция"
 				PerSocketData* userData = (PerSocketData*)ws->getUserData();
 				userData->name = "UNNAMED";
 				userData->user_id = last_user_id++;
-				count_user++;
-				cout << "New user connected, id = " << userData->user_id << endl;
-				cout << "Total user connected: " << count_user << endl;
 
-				ws->subscribe("user#" + to_string(userData->user_id)); //личный канал пользователя
-				ws->subscribe("broadcast"); //общий канал
+				updateName(userData);
+				ws->publish(BROADCAST_CHANNEL, online(userData->user_id));
+
+				cout << "\nNew user connected, id = " << userData->user_id << endl;
+				cout << "Total user connected: " << userNames.size() << endl;
+
+				string user_channel = "user#" + to_string(userData->user_id);
+
+				ws->subscribe(user_channel); //personal channel
+				ws->subscribe(BROADCAST_CHANNEL); //common channel
+
+				for (auto entry : userNames) {
+					ws->send(online(entry.first), uWS::OpCode::TEXT);
+				}
 			},
 			.message = [&last_user_id](auto* ws, string_view message, uWS::OpCode opCode) {
 				string strMessage = string(message);
@@ -71,23 +106,32 @@ int main() {
 						string text = parseUserText(strMessage);
 						string outgoingMessage = messageFrom(authorId, userData->name, text);
 
-						ws->publish("user#" + receiverId, outgoingMessage, uWS::OpCode::TEXT); //отправка получателю
-						ws->send("Message sent", uWS::OpCode::TEXT);
+						ws->publish("user#" + receiverId, outgoingMessage, uWS::OpCode::TEXT); //sending a message to the recipient
+						ws->send("Message sent", uWS::OpCode::TEXT);//message to send
 
 						cout << "Author #" << authorId << " wrote message to " << receiverId << endl;
 					}
-					else ws->send("Error, there is no user with ID = "+ receiverId, uWS::OpCode::TEXT);
+					else ws->send("Error, there is no user with ID = " + receiverId, uWS::OpCode::TEXT);
 				}
 				if (isSetName(strMessage)) {
 					string newName = parseName(strMessage);
-					if (newName.size() <= 255 && newName.find("::") == -1) {
+					if (newName.length() <= 255) {
 						userData->name = newName;
-						cout << "User  #" << authorId << " set their name" << endl;
+						updateName(userData);
+						ws->publish(BROADCAST_CHANNEL, online(userData->user_id));
+
+						cout << "\nUser  #" << authorId << " set their name" << endl;
 					}
+					else ws->send("Error, name too long", uWS::OpCode::TEXT);
 				}
 			},
-			.close = [](auto* /*ws*/, int /*code*/, string_view /*message*/) {
+			.close = [] (auto* ws, int /*code*/, string_view /*message*/) {
+				PerSocketData* userData = (PerSocketData*)ws->getUserData();
+				ws->publish(BROADCAST_CHANNEL, offline(userData->user_id));
+				deleteName(userData);
 
+				cout << "\nUser dtsconnected, id = " << userData->user_id << endl;
+				cout << "Total user connected: " << userNames.size() << endl;
 			}
 			})
 		/*Server start*/
